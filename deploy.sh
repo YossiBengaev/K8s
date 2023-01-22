9163a187cc5889738e41edd463ef1750967ddff0
YossiBengaev <yossibenga@gmail.com>
2023-01-22 20:34:18 +0200

    modify deploy.sh add global variables

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+# Global Variables
+RepoDockerHub='yossibenga/web-app:latest'
+MasterDir='/home/master'
+IngressFolder='Helm-yossi-ingress'
+IngressName='yossi-ingress'
+
 docker_run(){
     echo "running docker... "
-    ssh -o StrictHostKeyChecking=no "master@master" 'docker run -d yossibenga/web-app:latest'
+    ssh -o StrictHostKeyChecking=no "master@master" "docker run -d $RepoDockerHub"
 }
 
 copy_to_remote_machine() {
     echo "starting copy Helm Dir to Master...."
-    scp -r "Helm-yossi-ingress" "master@master":"/home/master" 
+    scp -r "$IngressFolder" "master@master":"$MasterDir" 
 }
 
 run_helm_yossi_ingress(){
     echo "starting deploy ingress.. "
-    ssh -o StrictHostKeyChecking=no "master@master" 'helm uninstall yossi-ingress' && 'helm install yossi-ingress Helm-yossi-ingress'
+    ssh -o StrictHostKeyChecking=no "master@master" "helm uninstall $IngressName && helm install $IngressName $IngressFolder"
 }
 
 docker_run
 copy_to_remote_machine 
 run_helm_yossi_ingress
+
+
