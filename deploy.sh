3af6e7363cde1a29097690b0bacd465c3580f7e1
YossiBengaev <yossibenga@gmail.com>
2023-01-22 18:45:55 +0200

    mod

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 
 docker_run(){
     echo "running docker run to my web-app... "
-    ssh -o StrictHostKeyChecking=no master@master 'docker run -d --name web-app $DockerHubRegistry:latest'
+    ssh -o StrictHostKeyChecking=no "master@master" 'docker run -d --name web-app $DockerHubRegistry:latest'
 }
 
 copy_to_remote_machine() {
     echo "starting copy Helm Dir to Master...."
-    scp -o StrictHostKeyChecking=no "Helm-yossi-ingress" master@master:"/home/master" 
+    scp -o StrictHostKeyChecking=no "Helm-yossi-ingress" "master@master":"/home/master" 
 }
 
 run_helm_yossi_ingress(){
     echo "starting deploy ingress.. "
-    ssh -o StrictHostKeyChecking=no master@master 'helm install yossi-ingress Helm-yossi-ingress'
+    ssh -o StrictHostKeyChecking=no "master@master" 'helm install yossi-ingress Helm-yossi-ingress'
 }
 
 docker_run
