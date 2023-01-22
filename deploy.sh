dcd0709882cfae324c7d8d43a2806977bd680cf0
YossiBengaev <yossibenga@gmail.com>
2023-01-22 21:22:24 +0200

    add spaces

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,18 @@ MasterDir='/home/master'
 IngressFolder='Helm-yossi-ingress'
 IngressName='yossi-ingress'
 
-docker_run(){
+docker_run() {
     echo "running docker... "
     ssh -o StrictHostKeyChecking=no "master@master" "docker run -d $RepoDockerHub"
 }
 
 copy_to_remote_machine() {
-    echo "starting copy Helm Dir to Master...."
+    echo "starting copy Helm Dir to Master..."
     scp -r "$IngressFolder" "master@master":"$MasterDir" 
 }
 
-run_helm_yossi_ingress(){
-    echo "starting deploy ingress.. "
+run_helm_yossi_ingress() {
+    echo "starting deploy ingress..."
     ssh -o StrictHostKeyChecking=no "master@master" "helm uninstall $IngressName ; helm install $IngressName $IngressFolder"
 }
 
